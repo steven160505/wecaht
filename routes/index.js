db0e010493d981350ff654ec0135d8c9204edd47
steven <limingshuy163.com>
2017-09-07 15:19:58 +0800

    Wechat verify

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require('crypto');
 
 /* GET home page. */
 router.get('/wechat/hello', function(req, res, next) {
-  res.render('index', { title: 'Hello, wechat!' });
+    console.log(req.body);
+    res.render('index', { title: 'Hello, wechat!' });
+});
+
+router.get('/wechat/verify', function(req, res, next) {
+
+    const { signature, timestamp, nonce, echostr } = req.query;
+
+    if (!signature || !timestamp || !nonce || !echostr) {
+        res.send("Invalid request!");
+    }
+
+    // 将token、timestamp、nonce三个参数进行字典序排序
+    const params = [token,timestamp, nonce];
+    params.sort();
+
+    // 将三个参数字符串拼接成一个字符串进行sha1加密
+    const str = params.join('');
+    const hash = crypto.createHash("sha1");
+    const sign = hash.update(str).digest("Hex");
+    if(sign === signature){
+      res.send(echostr);
+    }else{
+      res.send("Invalid signature!");
+    }
+
 });
 
 module.exports = router;
